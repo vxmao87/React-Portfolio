fbd6582c29e4ed5d47e0aaec8446d4c489e788e2
Vinh Mao <vinhxmao87@gmail.com>
2020-08-21 17:19:41 -0700

    Contact page populated, working on Portfolio section

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./style.css";
 
 function ContactCard() {
-    return null;
+    return (
+        <div className="contact-card">
+            <div className = "container">
+                <div className = "row">
+                    <div className = "col-md-8">
+                        <div className="page-header">
+                            <h1 className="contact-header">Contact</h1>
+                        </div>
+                        <form>
+                            <div className="form-group">
+                                <label for="exampleName">Name</label>
+                                <input type="name" className="form-control" id="exampleName2" aria-describedby="emailHelp" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleMail">Email</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleMessage">Message</label>
+                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            </div>
+                            <button type="submit" className="btn-sm btn-outline-secondary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ContactCard;
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -15,8 +15,8 @@ function ProjectCard(props) {
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="btn-group">
-                            <button type="button" className="btn btn-sm btn-outline-secondary">App</button>
-                            <button type="button" className="btn btn-sm btn-outline-secondary">GitHub</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary" src={props.appLink}>App</button>
+                            <button type="button" className="btn btn-sm btn-outline-secondary" src={props.githubLink}>GitHub</button>
                         </div>
                     </div>
                 </div>
