8a14d468435ef2f74231a125362a721c7a9a0faf
Vinh Mao <vinhxmao87@gmail.com>
2020-08-21 21:14:34 -0700

    Updates

---
+++ precache-manifest.7342815f15a0bcf3111695e6d252f977.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a153afe2d0f23219f7a4949089a01de",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "4ece5ba09b129f070a99",
+    "url": "/React-Portfolio/static/css/main.a7abcd01.chunk.css"
+  },
+  {
+    "revision": "67b294ce8a10b52648b4",
+    "url": "/React-Portfolio/static/js/2.99c880e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.99c880e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ece5ba09b129f070a99",
+    "url": "/React-Portfolio/static/js/main.bf5c73b8.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "f7ab7ff022b36b5f538579eb6c34e55b",
+    "url": "/React-Portfolio/static/media/DSC_0603.f7ab7ff0.jpeg"
+  },
+  {
+    "revision": "747484b40494d5ad974ecded26d6f5a7",
+    "url": "/React-Portfolio/static/media/better-earth.747484b4.png"
+  },
+  {
+    "revision": "277d318a4f57ddcd271f62b375813880",
+    "url": "/React-Portfolio/static/media/budget-tracker.277d318a.png"
+  },
+  {
+    "revision": "1ac2c42b7fdd42f7d52700107029fe5a",
+    "url": "/React-Portfolio/static/media/burger-logger.1ac2c42b.png"
+  },
+  {
+    "revision": "b95d77ba08887500e74f697b1ab53590",
+    "url": "/React-Portfolio/static/media/employee-directory.b95d77ba.png"
+  },
+  {
+    "revision": "dde7986491837c6901e50b7c29b405f9",
+    "url": "/React-Portfolio/static/media/employee-summary-webpage.dde79864.png"
+  },
+  {
+    "revision": "21985401095f8d940ab5b99c0958a997",
+    "url": "/React-Portfolio/static/media/note-taker.21985401.png"
+  },
+  {
+    "revision": "3574d983b26cacb99c905aa7dcb8b085",
+    "url": "/React-Portfolio/static/media/tinylearn.3574d983.png"
+  },
+  {
+    "revision": "25da0a647573b0f5fea8d5f4072b4b9b",
+    "url": "/React-Portfolio/static/media/treehouse-app.25da0a64.png"
+  },
+  {
+    "revision": "f748b3566b1d20fd050f038e5bd00e1e",
+    "url": "/React-Portfolio/static/media/weatherDashboard.f748b356.png"
+  }
+]);
